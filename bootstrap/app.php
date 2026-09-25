0a72610cbcc52ab18ba605ac1a2ed21c1c7eed9f
Neo83damian <148293408+Neo83damian@users.noreply.github.com>
2026-09-25 16:09:50 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+         $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
-           'admin' => \App\Http\Middleware\EnsureAdmin::class,
-        ]);
+        'admin' => \App\Http\Middleware\EnsureAdmin::class,
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
